8ba1d386a3dcd69c7d6f0c0fc5e65f9e139f45a3
Rajneeshgupta73 <rajneeshgupta73@gmail.com>
2024-09-30 22:49:17 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button"
+
+
+
+function App() {
+
+
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
